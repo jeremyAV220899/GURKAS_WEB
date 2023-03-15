58711852f5ab5a531d66e8c245df26702e6245fd
jeremyAV220899 <jeremy232129@gmail.com>
2023-03-15 16:26:02 -0500

    usuario

--- app/Http/Controllers/Administrador/UsuarioController.php
+++ app/Http/Controllers/Administrador/UsuarioController.php
@@ -3,13 +3,64 @@
 namespace App\Http\Controllers\Administrador;
 
 use App\Http\Controllers\Controller;
-use App\Models\Administrador\Sistema\Usuario\Usuario;
+use App\Models\Administrador\Sistema\Estado;
+use App\Models\Administrador\Sistema\Usuario;
+use App\Models\RRHH\Personal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Response;
 
 class UsuarioController extends Controller
 {
     public function index()
     {
-        return view('administrador.usuario');
+       
+        $datos = [
+            'usuarios' => Usuario::all(),
+            'estados' => Estado::all(),
+            'empleados' => Personal::all(),
+        ];
+        return view('administrador.usuario',$datos);
+    }
+    public function store (Request $request){
+        $usuario = new Usuario();
+        $usuario->dni=$request->dni;
+        $usuario->name=$request->name;
+        $usuario->idestado=$request->idestado;
+        $usuario->email=$request->email;
+        $usuario->password=Hash::make($request->password);
+        $usuario->save();
+        return redirect()->route('administrador.sistemas.usuario');
+    }
+
+    public function edit($id){
+        $usuario = Usuario::find($id);
+        $datos = [
+            'usuario' => $usuario,
+        ];
+        return Response::json($datos);
+    }
+
+    public function update ($id, Request $request){
+        $usuario = Usuario::find($id);
+        $usuario->dni=$request->dni;
+        $usuario->name=$request->name;
+        $usuario->idestado=$request->idestado;
+        $usuario->email=$request->email;
+        $usuario->password=Hash::make($request->password);
+        $usuario->save();
+        return redirect()->route('administrador.sistemas.usuario');
+    }
+
+    public function delete($id){
+        try {
+            $usuario = Usuario::find($id);
+            $usuario->delete();
+            $response = 'ok';
+            return Response::json($response);
+        } catch (\Throwable $th) {
+            $response = 'error';
+            return Response::json($response);
+        }
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('dni');
             $table->string('name');
+            $table->integer('idestado');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
